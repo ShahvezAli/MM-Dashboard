0de0ea96b73490723a0e29d440acc0f119b49366
Ubuntu <ubuntu@ip-172-31-13-102.us-east-2.compute.internal>
2021-11-08 17:02:42 +0000

    New model colleges mentee add

--- models/staging/mm_mentees.sql
+++ models/staging/mm_mentees.sql
@@ -1,4 +1,6 @@
 with source_data as (
-    select * from {{ source('dashboard_data_set', 'mm_users')}}
+    select * from {{ source('dashboard_data_set', 'mm_users')}} users
+    inner join  {{ source('dashboard_data_set', 'mm_college_mentees')}} college_mentees
+    on college_mentees.mentee_id =  users.id
 )
-select * from source_data where user_type = 'Mentee'
+select * from source_data
